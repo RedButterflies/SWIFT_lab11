8b0753bf2d5b6beb1925a362b86b70542d98e70e
Aleksandra Olejarz <106548989+RedButterflies@users.noreply.github.com>
2024-01-24 18:04:40 +0100

    Update StudentNaErasmusie.swift

--- StudentNaErasmusie.swift
+++ StudentNaErasmusie.swift
@@ -1,86 +1,75 @@
 //
 //  StudentNaErasmusie.swift
-//  SWIFT_klasy_lab11
+//  zad11
 //
-//  Created by admin on 1/8/24.
+//  Created by admin on 1/18/24.
 //
 
 import Foundation
 class StudentNaErasmusie:Student
 {
-    var nazwaUczelniZaGranica: String = ""
-    var dataRozpoczecia: String = "2003-12-01"
-    var dataZakonczenia: String = "2005-07-15"
-    var kursy: [String:ocena] = ["English":ocena.bdb]
-    
-    init(imie:String, nazwisko:String, rok_urodzenia:Int, numerIndeksu:Int, kierunekStudiow:kierunek,rokStudiow:Int,oceny:[ocena],nazwaUczelniZaGranica: String, dataRozpoczecia : String,dataZakonczenia:String,    kursy: [String:ocena])
-    {
-        super.init(imie: imie, nazwisko: nazwisko, rok_urodzenia: rok_urodzenia, numerIndeksu: numerIndeksu, kierunekStudiow: kierunekStudiow, rokStudiow: rokStudiow, oceny: oceny)
+    var nazwaUczelniZaGranica:String = ""
+    var dataRozpoczeciaErasmusa:String = ""
+    var dataZakonczeniaErasmusa:String = ""
+    var kursy:[String:Oceny] = ["Angielski":Oceny.bdb]
+    init(imie: String, nazwisko: String, rokUrodzenia: Int, nrIndeksu: Int, kierunekStudiow: kierunek, rokStudiow: Int, oceny: [Oceny], nazwaUczelniZaGranica:String, dataRozpoczeciaErasmusa:String, dataZakonczeniaErasmusa:String,kursy:[String:Oceny]) {
+        super.init(imie: imie, nazwisko: nazwisko, rokUrodzenia: rokUrodzenia, nrIndeksu: nrIndeksu, kierunekStudiow: kierunekStudiow, rokStudiow: rokStudiow, oceny: oceny)
         self.nazwaUczelniZaGranica = nazwaUczelniZaGranica
-        self.dataRozpoczecia = dataRozpoczecia
-        self.dataZakonczenia = dataZakonczenia
+        self.dataRozpoczeciaErasmusa = dataRozpoczeciaErasmusa
+        self.dataZakonczeniaErasmusa = dataZakonczeniaErasmusa
         self.kursy = kursy
+        
     }
-    
-    func liczbaDniNaErasmusie() -> Int
+    func dniNaErasmusie()->Int
     {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        guard let startDate = dateFormatter.date(from:dataRozpoczecia),
-        let endDate = dateFormatter.date(from: dataZakonczenia)
-        else {
-            fatalError("Nieprawidlowe daty")
-            
-        }
-        
+        guard let startDate = dateFormatter.date(from: dataRozpoczeciaErasmusa),let endDate = dateFormatter.date(from: dataZakonczeniaErasmusa)
+                else
+                {
+                    fatalError("Nieprawidlowe daty")
+                }
         let spedzonyCzasNaErasmusie = endDate.timeIntervalSince(startDate)
-        let liczbaDni = Int(ceil((spedzonyCzasNaErasmusie / (60*60*24))))
-        //print("Liczba dni spedzonych na Erasmusie:  \(liczbaDni)")
+        let liczbaDni = Int(ceil((spedzonyCzasNaErasmusie/(60*60*24))))
         return liczbaDni
     }
-    
-    func ocenaStudenta() -> String
+    func ocenaZKursow()->String
     {
-        let oceny = kursy.map{$0.value.rawValue}
-        let suma = oceny.reduce(0.0,+)
-        let srednia = suma/Double(kursy.count)
-        print("Srednia studenta: \(srednia)")
+        let srednia = Double( kursy.map{$0.value}.map{$0.rawValue}.reduce(0,+))/Double(kursy.count)
         switch(srednia)
         {
-        case let x where x > 4.6:
-            //print("Ocena bardzo dobra")
+        case let x where x >= 4.6:
             return "bardzo dobra"
-        case let x where x > 3.6:
-            //print("Ocena dobra")
+        case let x where x >= 3.6:
             return "dobra"
-        case let x where x > 3.0:
-            //print("Ocena dostateczna")
+        case let x where x >= 3.0:
             return "dostateczna"
         default:
-            //print("Ocena niedostateczna")
             return "niedostateczna"
         }
-        
-        
-    }
-    override func wyswietl() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let startDate = dateFormatter.date(from:dataRozpoczecia)
-        let endDate = dateFormatter.date(from: dataZakonczenia)
-        
-        
-        super.wyswietl()
-        print("Nazwa uczelni za granica: \(nazwaUczelniZaGranica)")
-        print("Data rozpoczecia Erasmusa: \(startDate!)")
-        print("Data zakonczenia Erasmusa: \(endDate!)")
-        print("Kursy studenta: ")
-        for i in kursy{
-            print("Nazwa: \(i.key) Ocena: \(i.value)")
-        }
-        print("\nLiczba dni na Erasmusie: \(liczbaDniNaErasmusie())")
-        print("\nOcena studenta: \(ocenaStudenta())")
     }
     
+
+
+    override func wyswietl() {
+        print("\n------------------")
+        super.wyswietl()
+        print("Nazwa uczelni za granica: \(nazwaUczelniZaGranica)")
+        print("Data rozpoczecia Erasmusa: \(dataRozpoczeciaErasmusa)")
+        print("Data zakonczenia Erasmusa: \(dataZakonczeniaErasmusa)")
+        print("Kursy")
+        for i in kursy.indices
+        {
+            print("\n------\n")
+            print("""
+                Nazwa kursu: \(kursy[i].key)
+                Ocena z kursu: \(kursy[i].value)
+                """)
+        }
+        print("Dni spedzone na Erasmusie: \(dniNaErasmusie())")
+        print("Ocena studenta ze wszystkich kursow: \(ocenaZKursow())")
+    }
 }
+   
+
 
