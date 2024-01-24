b4fca924ed06d1d332b7349d045e6fa75949f9f0
Aleksandra Olejarz <106548989+RedButterflies@users.noreply.github.com>
2024-01-24 18:03:45 +0100

    Update Prostokat.swift

--- Prostokat.swift
+++ Prostokat.swift
@@ -1,36 +1,32 @@
 //
 //  Prostokat.swift
-//  SWIFT_klasy_lab11
+//  zad11
 //
-//  Created by admin on 1/7/24.
+//  Created by admin on 1/17/24.
 //
 
 import Foundation
-
 class Prostokat
 {
-    var bokA : Double = 3.0
-    var bokB : Double = 4.0
-    
-    init(bokA:Double, bokB:Double)
+    var bokA_len: Double
+    var bokB_len: Double
+    init(bokA_len:Double,bokB_len:Double) {
+        self.bokA_len = bokA_len
+        self.bokB_len = bokB_len
+    }
+    func pole()->Double
     {
-        self.bokA = bokA
-        self.bokB = bokB
+        return bokA_len * bokB_len
     }
-    
-    func getArea()->Double{
-        let area = bokA*bokB
-        return area
-    }
-    func getObwod()->Double{
-        let obwod = bokA*2 + bokB*2
-        return obwod
+    func obwod()->Double
+    {
+        return 2*(bokA_len+bokB_len)
     }
     func wyswietl()
     {
-        print("Dlugosc boku A: \(bokA)")
-        print("Dlugosc boku B: \(bokB)")
-        print(String(format:"Pole prostokata: %.2f",getArea()))
-        print(String(format:"Obwod prostokata: %.2f",getObwod()))
+        print("Dlugosc boku A: \(bokA_len)")
+        print("Dlugosc boku B: \(bokB_len)")
+        print("Obwod: \(obwod())")
+        print("Pole: \(pole())")
     }
 }
