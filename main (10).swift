951beb6e6e661f43bb0b67ca15776c367bb01278
Aleksandra Olejarz <106548989+RedButterflies@users.noreply.github.com>
2024-01-24 18:06:27 +0100

    Update main (10).swift

--- main (10).swift
+++ main (10).swift
@@ -1,294 +1,261 @@
 //
 //  main.swift
-//  SWIFT_klasy_lab11
+//  zad11
 //
-//  Created by admin on 1/7/24.
+//  Created by admin on 1/17/24.
 //
 
 import Foundation
 
-var prostokatA = Prostokat(bokA:10,bokB:15.5)
-prostokatA.wyswietl()
-print("Funkcja obwod:\(prostokatA.getObwod())")
-print("Funkcja pole: \(prostokatA.getArea())")
-
-print("------------------\n")
-var prostopadloscianA = Prostopadloscian(bokA:30,bokB:40,wysokosc: 10)
-prostopadloscianA.wyswietl()
-print("Funkcja pole calkowite: \(prostopadloscianA.poleBryly())")
-print("Funkcja suma krawedzi: \(prostopadloscianA.sumaKrawedzi())")
-print("Funkcja objetosc: \(prostopadloscianA.objetoscBryly())")
-print("------------------\n")
+var prostokat = Prostokat(bokA_len: 12, bokB_len: 4)
+print("\n")
+prostokat.wyswietl()
+print("\n")
+var prostopadloscian = Prostopadloscian(bokA_len: 3, bokB_len: 6, wysokosc: 10)
+prostopadloscian.wyswietl()
 
 
 
-print("Podaj liczbe studentow: ")
-var wczytLiczba = readLine()!
-while(Int(wczytLiczba)==nil || Int(wczytLiczba)! < 0)
-{
-    print("Wprowadzono niepoprawna liczbe studentow, wprowadz poprawna liczbe: ")
-    wczytLiczba = readLine()!
-}
-var liczba = Int(wczytLiczba)!
+//11.3
+var studentJacek = Student(imie: "Jacek", nazwisko: "Placek", rokUrodzenia: 2005, nrIndeksu: 3908, kierunekStudiow: kierunek.Informatyka, rokStudiow: 5, oceny: [Oceny.dbplus,Oceny.bdb,Oceny.db,Oceny.dbplus,Oceny.dst])
 
-
-
-var tablicaStudentow = Array(repeating: Student(imie: "", nazwisko: "", rok_urodzenia: 2005, numerIndeksu: 1, kierunekStudiow: kierunek.Informatyka, rokStudiow: 1, oceny: [ocena.bdb, ocena.bdb, ocena.db, ocena.bdb, ocena.dbplus]), count: liczba)
-
-
-
-
+studentJacek.wyswietl()
 
 
 func wczytajRokUrodzenia()->Int
 {
-    print("Wprowadz rok urodzenia:")
-    var wczytRok = readLine()!
-    while(Int(wczytRok)==nil || Int(wczytRok)! < 1950)
+    print("Wprowadz rok urodzenia studenta: ")
+    var wrok = readLine()!
+    while(Int(wrok)==nil || Int(wrok)! < 1970)
     {
-        print("Wprowadzono niepoprawny rok urodzenia. Wprowadz poprawny rok urodzenia: ")
-        wczytRok = readLine()!
+        print("Wprowadzono niepoprawny rok urodzenia. Wprowadz rok urodzenia studenta: ")
+        wrok = readLine()!
     }
-    let rok = Int(wczytRok)!
+    let rok = Int(wrok)!
     return rok
 }
-func wczytajKierunekStudiow()->kierunek
+func wczytajKierunkeStudiow()->kierunek
 {
-    print("Wprowadz kierunek studiow: ")
-    var wczytKierunek = readLine()!
-    while(wczytKierunek != "Informatyka" && wczytKierunek != "Matematyka" && wczytKierunek != "Filozofia" && wczytKierunek != "Elektrotechnika")
-    {
-        print("Wprowadzono niepoprawny kierunek studiow. Wprowadz poprawny kierunek: ")
-        wczytKierunek = readLine()!
+    print("Podaj kierunek studiow studenta: ")
+    print("""
+Wybierz jedna z opcji
+1-Informatyka
+2-Matematyka
+3-Filozofia
+4-Elektrotechnika
+""")
+    var wybor = readLine()!
+    while(Int(wybor)==nil || (Int(wybor)! != 1 && Int(wybor)! != 2 && Int(wybor)! != 3 && Int(wybor)! != 4))
+          {
+        print("Wprowadzono nieprawidlowa warosc. Wybierz jedna z opcji: ")
+        wybor = readLine()!
     }
-    let kierx = wczytKierunek
-    switch (kierx)
+    let k = Int(wybor)!
+   
+    switch(k)
     {
-    case "Informatyka":
+    case 1:
         return kierunek.Informatyka
-    case "Elektrotechnika":
-        return kierunek.Elektrotechnika
-    case "Filozofia":
-        return kierunek.Filozofia
-    case "Matematyka":
+    case 2:
         return kierunek.Matematyka
-    default:
-        print("Wprowadzono nieprawidlowy kierunek")
+    case 3:
         return kierunek.Filozofia
+    case 4:
+      return kierunek.Elektrotechnika
+    default:
+        return kierunek.Informatyka
     }
-    
 }
 func wczytajRokStudiow()->Int
 {
-    print("Wprowadz rok studiow: ")
-    
-    var wczytRok = readLine()!
-    while(Int(wczytRok)==nil || (Int(wczytRok)! != 1 && Int(wczytRok)! != 2 && Int(wczytRok)! != 3 && Int(wczytRok)! != 4 && Int(wczytRok)! != 5))
+    print("Wprowadz rok studiow studenta: ")
+    var wrok = readLine()!
+    while(Int(wrok)==nil || (Int(wrok)!<=0 || Int(wrok)!>5))
     {
-        print("Wprowadzono niepoprawny rok studiow. Wprowadz poprawny rok studiow: ")
-        wczytRok = readLine()!
+        print("Wprowadzono niepoprawny rok studiow. Wprowadz rok studiow studenta: ")
+        wrok = readLine()!
     }
-    let rok = Int(wczytRok)!
+    let rok = Int(wrok)!
     return rok
+}
+func wczytajOcenyStudenta()->[Oceny]
+{
+    var tabOcen = Array(repeating: Oceny.bdb, count: 5)
+    print("""
+Wartosciami ocen moga byc
+2.0
+3.0
+3.5
+4.0
+4.5
+5.0
+""")
+    for i in tabOcen.indices
+    {
+        print("Wprowadz ocene \(i). studenta: ")
+        var wocena = readLine()!
+        while(Double(wocena)==nil || Oceny.init(rawValue: Double(wocena)!)==nil)
+        {
+            print("Wprowadzono niepoprawna ocene. Wprowadz ocene \(i). studenta: ")
+            wocena = readLine()!
+        }
+        tabOcen[i] = Oceny.init(rawValue: Double(wocena)!)!
+    }
+    return tabOcen
+    
+}
+func wczytajImieStudenta()->String
+{
+    print("Wprowadz imie studenta: ")
+    let imie = readLine()!
+    return imie
+}
+func wczytajNazwiskoStudenta()->String
+{
+    print("Wprowadz nazwisko studenta: ")
+    let nazwisko = readLine()!
+    return nazwisko
+    
+}
+func wczytajNrIndeksuStudenta()->Int
+{
+    print("Wprowadz numer indeksu studenta: ")
+    var wNrIn = readLine()!
+    while(Int(wNrIn)==nil || Int(wNrIn)! <= 0)
+    {
+        print("Wprowadzono niepoprawny numer indeksu. Wprowadz numer indeksu studenta: ")
+        wNrIn = readLine()!
+    }
+    return Int(wNrIn)!
+}
+func wczytajDaneStudenta()->Student
+{
+   return Student(imie: wczytajImieStudenta(), nazwisko: wczytajNazwiskoStudenta(), rokUrodzenia: wczytajRokUrodzenia(), nrIndeksu: wczytajNrIndeksuStudenta(), kierunekStudiow: wczytajKierunkeStudiow(), rokStudiow: wczytajRokStudiow(), oceny: wczytajOcenyStudenta())
+}
+
+func wyswietlStudentaNaKierunku(a: [Student],b:kierunek)
+{
+    print("\nStudenci na kierunku \(b): \n")
+    for i in a.indices
+    {
+        if(a[i].kierunekStudiow == b)
+        {
+            a[i].wyswietl()
+            print("\n")
+        }
+    }
+    print("\n")
     
 }
 
-func wczytajOcenyStudenta()->[ocena]
+func stworzTabliceStudentow(n:Int)->[Student]
 {
-    var tablica = Array(repeating: ocena.db, count: 5)
-    for i in tablica.indices{
-        print("Wprowadz ocene w formacie slownym\(i).: ")
-        var wczytOcena = readLine()!
-    while(wczytOcena != "ndst" && wczytOcena != "dost" && wczytOcena != "dostplus" && wczytOcena != "db" && wczytOcena != "dbplus" && wczytOcena != "bdb")
+    var tablica = Array(repeating: Student(imie: "", nazwisko: "", rokUrodzenia: 1999, nrIndeksu: 1111, kierunekStudiow: kierunek.Informatyka, rokStudiow: 1, oceny: [Oceny.db,Oceny.bdb,Oceny.bdb,Oceny.dbplus,Oceny.bdb]), count: n)
+    for i in tablica.indices
     {
-        print("Wprowadzono niepoprawna ocene. Wprowadz poprawna ocene: ")
-        wczytOcena = readLine()!
-    }
-    let ocenax = wczytOcena
-    switch (ocenax)
-    {
-    case "bdb":
-        tablica[i] = ocena.bdb
-    case "dbplus":
-        tablica[i] = ocena.dbplus
-    case "db":
-        tablica[i] = ocena.db
-    case "dostplus":
-        tablica[i] = ocena.dostplus
-    case "dost":
-        tablica[i] = ocena.dost
-    case "ndst":
-        tablica[i] = ocena.ndst
-    default:
-        tablica[i] = ocena.bdb
-    }
+        tablica[i] = Student(imie: wczytajImieStudenta(), nazwisko: wczytajNazwiskoStudenta(), rokUrodzenia: wczytajRokUrodzenia(), nrIndeksu: wczytajNrIndeksuStudenta(), kierunekStudiow: wczytajKierunkeStudiow(), rokStudiow: wczytajRokStudiow(), oceny: wczytajOcenyStudenta())
     }
     return tablica
     
 }
-func wczytajDaneStudenta()->Student
-{
-    print("Wprowadz imie: ")
-    let imie = readLine()!
-    print("Wprowadz nazwisko: ")
-    let nazwisko = readLine()!
-    print("Wprowadz numer indeksu: ")
-    var wprNrIndeksu = readLine()!
-    while(Int(wprNrIndeksu) == nil || Int(wprNrIndeksu)! < 0)
-    {
-        print("Wprowadzono niepoprawny numer indeksu. Wprowadz poprawny numer indeksu: ")
-        wprNrIndeksu = readLine()!
-    }
-    
-    let nrIndeksu = Int(wprNrIndeksu)!
-    
-    let student = Student(imie: imie, nazwisko: nazwisko, rok_urodzenia: wczytajRokUrodzenia(), numerIndeksu: nrIndeksu, kierunekStudiow: wczytajKierunekStudiow(), rokStudiow: wczytajRokStudiow(), oceny: wczytajOcenyStudenta())
-    return student
-}
 
-func wyswietlStudentaZKierunku()
+func podajLiczbeStudentow()->Int{
+print("Podaj liczbe studentow: ")
+var wliczbaS = readLine()!
+while(Int(wliczbaS)==nil || Int(wliczbaS)! <= 0)
 {
-    
-    print("Wprowadz kierunek studiow: ")
-    var wczytKierunek = readLine()!
-    while(wczytKierunek != "Informatyka" && wczytKierunek != "Matematyka" && wczytKierunek != "Filozofia" && wczytKierunek != "Elektrotechnika")
-    {
-        print("Wprowadzono niepoprawny kierunek studiow. Wprowadz poprawny kierunek: ")
-        wczytKierunek = readLine()!
-    }
-    let kierx = wczytKierunek
-    print("Studenci kierunku: ")
-    switch (kierx)
-    {
-    case "Informatyka":
-       
-        for i in tablicaStudentow{
-            if(i.kierunekStudiow == kierunek.Informatyka)
-            {
-                i.wyswietl()
-            }
-                
-        }
-    case "Elektrotechnika":
-        
-         for i in tablicaStudentow{
-             if(i.kierunekStudiow == kierunek.Elektrotechnika)
-             {
-                 i.wyswietl()
-             }
-                 
-         }
-    case "Filozofia":
-        
-         for i in tablicaStudentow{
-             if(i.kierunekStudiow == kierunek.Filozofia)
-             {
-                 i.wyswietl()
-             }
-                 
-         }    case "Matematyka":
-        
-         for i in tablicaStudentow{
-             if(i.kierunekStudiow == kierunek.Matematyka)
-             {
-                 i.wyswietl()
-             }
-                 
-         }
-    default:
-        print("Wprowadzono nieprawidlowy kierunek studiow")
-        
-    }
+    print("Wprowadzono nieprawidlowa liczbe studentow. Wprowadz liczbe studentow: ")
+    wliczbaS = readLine()!
 }
-
-for i in tablicaStudentow.indices
+    return Int(wliczbaS)!
+}
+func podajLiczbeKursow()->Int{
+print("Podaj liczbe kursow: ")
+var wliczbaS = readLine()!
+while(Int(wliczbaS)==nil || Int(wliczbaS)! <= 0)
 {
-    //tablicaStudentow[i] = wczytajDaneStudenta()
+    print("Wprowadzono nieprawidlowa liczbe kursow. Wprowadz liczbe kursow: ")
+    wliczbaS = readLine()!
+}
+    return Int(wliczbaS)!
 }
 
 
-
-print("\n\n\n")
-print("Dane studentow: ")
-for i in tablicaStudentow.indices
+//print("\n\n")
+//let tablicaStudentow = stworzTabliceStudentow(n: podajLiczbeStudentow())
+//
+//for i in tablicaStudentow.indices
+//{
+//    print("\n--------------\n")
+//    tablicaStudentow[i].wyswietl()
+//
+//}
+func wczytajNazweUczelniZaGranica()->String
 {
-    //tablicaStudentow[i].wyswietl()
-}
-
-//wyswietlStudentaZKierunku()
-func wczytajKursy() -> [String: ocena] {
-    var kursy: [String: ocena] = [:]
-
-    print("Wprowadz kursy studenta (nazwa kursu: ocena):")
-    
-    while true {
-        print("Wprowadz nazwe kursu (lub wpisz 'koniec' aby zakonczyc):")
-        let nazwaKursu = readLine()!
-
-        if nazwaKursu.lowercased() == "koniec" {
-            break
-        }
-
-        print("Wprowadz ocene dla kursu w formacie numerycznym \(nazwaKursu):")
-        let ocenaString = readLine()!
-
-        if let ocenaDouble = Double(ocenaString), let ocenax = ocena(rawValue: ocenaDouble) {
-            kursy[nazwaKursu] = ocenax
-        } else {
-            print("Nieprawidlowa ocena. Wprowadz ponownie nazwe kursu i poprawna ocene.")
-        }
-    }
-
-    return kursy
-}
-
-
-func wczytajDaneStudentaNaErasmusie()->StudentNaErasmusie
-{
-    print("Wprowadz imie: ")
-    let imie = readLine()!
-    print("Wprowadz nazwisko: ")
-    let nazwisko = readLine()!
-    print("Wprowadz numer indeksu: ")
-    var wprNrIndeksu = readLine()!
-    while(Int(wprNrIndeksu) == nil || Int(wprNrIndeksu)! < 0)
-    {
-        print("Wprowadzono niepoprawny numer indeksu. Wprowadz poprawny numer indeksu: ")
-        wprNrIndeksu = readLine()!
-    }
-    
-    let nrIndeksu = Int(wprNrIndeksu)!
-    
     print("Wprowadz nazwe uczelni za granica: ")
-    let nazwaUczelniZaGranica = readLine()!
-    
+    var uczelniaZaGranica = readLine()!
+    return uczelniaZaGranica
+}
+func wczytajDateRozpoczeciaErasmusa()->String
+{
+    print("Wprowadz date rozpoczecia Erasmusa w formacie yyyy-MM-dd: ")
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd"
-    print("Wprowadz date rozpoczecia Erasmusa w formacie yyyy-MM-dd: ")
-    var wprDataStart = readLine()!
-    while( dateFormatter.date(from:wprDataStart ) == nil
-           )
-           {
-        print("Wprowadzono niepoprawna date. Wprowadz poprawna date w formacie yyy-MM-dd: ")
-        wprDataStart = readLine()!
+    var data = readLine()!
+    while(dateFormatter.date(from: data)==nil)
+    {
+        print("Wprowadzono nieprawidlowa date. Wprowadz date w formacie yyyy-MM-dd: ")
+        data = readLine()!
     }
-    let dataStart = wprDataStart
-    
-    
+    return data
+}
+func wczytajDateZakonczeniaErasmusa()->String
+{
     print("Wprowadz date zakonczenia Erasmusa w formacie yyyy-MM-dd: ")
-    var wprDataEnd = readLine()!
-    while( dateFormatter.date(from:wprDataEnd ) == nil
-           )
-           {
-        print("Wprowadzono niepoprawna date. Wprowadz poprawna date w formacie yyy-MM-dd: ")
-        wprDataEnd = readLine()!
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    var data = readLine()!
+    while(dateFormatter.date(from: data)==nil)
+    {
+        print("Wprowadzono nieprawidlowa date. Wprowadz date w formacie yyyy-MM-dd: ")
+        data = readLine()!
     }
-    let dataEnd = wprDataEnd
+    return data
+}
+func wczytajKursy(n:Int)->[String:Oceny]
+{
+    var kursy:[String:Oceny] = [:]
+    for _ in stride(from: 0, to: n, by: 1)
+    {
+        print("Wprowadz nazwe kursu: ")
+        let kurs = readLine()!
+        print("""
+            Oceny moga przyjmowac wartosci:
+            5.0
+            4.5
+            4.0
+            3.5
+            3.0
+            2.0
+            """)
+        print("Wprowadz ocene za kurs \(kurs) ")
+        var wocena = readLine()!
+        while(Double(wocena)==nil || Oceny.init(rawValue: Double(wocena)!)==nil)
+        {
+            print("Wprowadzono niepoprawna ocene. Wprowadz poprawna ocene: ")
+            wocena = readLine()!
+        }
+        let ocena = Oceny.init(rawValue: Double(wocena)!)
+        kursy[kurs]=ocena
+                    
+    }
+    return kursy
     
-    
-    let student = StudentNaErasmusie(imie: imie, nazwisko: nazwisko, rok_urodzenia: wczytajRokUrodzenia(), numerIndeksu: nrIndeksu, kierunekStudiow: wczytajKierunekStudiow(), rokStudiow: wczytajRokStudiow(), oceny: wczytajOcenyStudenta(),nazwaUczelniZaGranica: nazwaUczelniZaGranica,dataRozpoczecia: dataStart,dataZakonczenia: dataEnd,kursy: wczytajKursy())
-    return student
 }
 
-var studentNaErasmusie = wczytajDaneStudentaNaErasmusie()
-studentNaErasmusie.wyswietl()
+
+
+var Erik = StudentNaErasmusie(imie: wczytajImieStudenta(), nazwisko: wczytajNazwiskoStudenta(), rokUrodzenia: wczytajRokUrodzenia(), nrIndeksu: wczytajNrIndeksuStudenta(), kierunekStudiow: wczytajKierunkeStudiow(), rokStudiow: wczytajRokStudiow(), oceny: wczytajOcenyStudenta(), nazwaUczelniZaGranica: wczytajNazweUczelniZaGranica(), dataRozpoczeciaErasmusa: wczytajDateRozpoczeciaErasmusa(), dataZakonczeniaErasmusa: wczytajDateZakonczeniaErasmusa(), kursy: wczytajKursy(n: podajLiczbeKursow()))
+
+Erik.wyswietl()
+
